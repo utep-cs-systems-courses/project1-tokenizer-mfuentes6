2a0579b54648a353e136b02325b409da887824a9
student <student@systems-vm.localdomain>
2020-09-23 00:52:18 -0600

    changes to main and tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,47 +1,52 @@
 #include <stdio.h>
 #include "tokinizer.h"
 
-
+//checks if character is a space or not
 int space_char (char c) {
-  if (c == "\t" || c == " "){
+  if (c == '\t' || c == ' '){
     return 1;
   }
 
   return 0;
 }
 
+//checks if the character is non space
 int non_space_char(char c) {
-  if (c == "\t" || c == " ") {
+  if (c == '\t' || c == ' ') {
     return 0;
   }
 
   return 1;
 }
 
+//iterates untill a character is found
 char word_start(char *str) {
   char *p = str;
-
-  for (int i = 0; i < strlen(str) +1; i++) {
-    if (p[i] == "\0") {
-      return[i];
-    }
-    if (p[i] != " " || p[i] != "\t") {
+  int i = 0;
+  if (p[i] == '\0'){
+    return p[i];
+  }
+  while (p[i] != '\0') {
+    if (non_space_char(p[i])==1) {
       return p[i];
     }
-
+    i++
   }
 
 }
 
 char *word_terminator(char *word){
   char *p = word;
-  if (p +1 == "\0"){
+  if (p +1 == '\0'){
     return p+1;
   }
-  for (int i =0; i < strlen(word)+1; i++){
-    if (p[i] == " " || p[i] == "\t"){
+  int i = 0;
+  while (p[i] != '\0'){
+    if (space_char(p[i])==1){
       return p[i];
     }
+    i++;
+
   }
 }
 
@@ -49,28 +54,80 @@ int count_words(char *str) {
   int count = 0;
   char *p = str;
 
-  for (int i = 0; i < strlen(str)+1; i++){
-    if (p[i]== " " || p[i] == "\t") {
-      count ++;
+  int i = 0;
+  while (p[i] != '\0'){
+    if (space_char(p[i])==1){
+      count++;
     }
+    i++;
   }
+  return count;
+  
 }
 
 
+char *copy_str(char *inStr, short len){
+  char *word = (char*) malloc(len+1);
+  char *temp = word;
+
+
+  //continues till null character
+  while (*intStr != '\0') {
+    *temp = *inStr;  //continues copying each character
+    temp++;
+    inStr++;
+
+  }
+
+  return word; //returns copied string
+}
+
 char **tokenizer (char *str){
+  char *temp = str;
+  int word_count = count_words(*str);
+  word_count += 2;
 
+  char **token = (char**)malloc(word_count * sizeof(char*));
+  int count = 0;
 
+  int start = 0;
+  int end = 0;
+
+  while(str[end] != '\0'){
+    if(space_char(str[end]) == 1){
+      token[count++] = copy_str(str+start, end-start);
+      start = end +1;
+      end = start;
+      count++
+    }
+    else {
+      end++;
+    }
+
+  }
+  token[count++] = copy_str(str+start,end-start);
+  token[count] = "";
+
+  return token;
 }
 
 
 void print_tokens(char **tokens){
+  while (*str != ""){
+    printf("%s\n", *str);
+    str++;
 
+  }
 
 }
 
 
 void free_tokens(char **tokens) {
-
-
+  char **temp = str;
+  while(*temp != ""){
+    free (*temp);
+    temp++;
+  }
+  free(temp);
 
 }
--- src/uimain.c
+++ src/uimain.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
-
+#include "tokenizer.h"
+#include "history.h"
 
 
 int main ()
 {
-  puts("Welcome to the Tokenizer!");
+  printf("Welcome to the Tokenizer!\n");
+  Char str[200];
+  Char *p = str;
+  printf("Here are your options\n");
+  printf("1. Enter a string to tokenize\n");
+  printf("2. Enter q to exit program\n");
+  printf("3. Enter h to see your history\n");
+  printf("4. Enter ! followed by a number to acces exact history");
+  int i = 0;
+  int k = 0;
 
-  
-  while (1)
-    {
-      Char str[200];
-      Char *p = str;
-      puts("Please enter some strings, enter 'q' when done\n");
-      
-      
-      goto done;
+  while(k == 0){
+
+
+    while( (c = getchar()) != '\n') {
+      str[i] = c;
+      i++;
+
+    }
+    str[i] = '\0';
+    if (str[0] == 'q'){
+      gotto done;
+    }
+    else if(str[0] == 'h'){
 
     }
 
- done:
-  return 0;
+    else if(str[0] == '!'){
+
+    }
+
+    else{
+      tokens = tokenizer(str);
+      print_tokens(tokens);
+      //add history function
+    }
+    
+
+
+
+
+
+  }
+
+  done:
+   printf("Goodbye!");
+   return 0;
 
 }
